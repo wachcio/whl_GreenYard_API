58742dab3add5eb699beb06e6b2e382d65a714dc
wachcio <wachcio@poczta.fm>
2021-06-07 02:14:28 +0200

    working hours entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,29 +10,6 @@ import { envConfigFilePath } from './utils/env-config-file-path';
 import { CronModule } from './cron/cron.module';
 import { WorkingHoursModule } from './working-hours/working-hours.module';
 
-console.log({
-  type: 'mysql',
-  host: process.env.TYPEORM_HOST,
-  port: Number(process.env.TYPEORM_PORT),
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-  entities: [process.env.TYPEORM_ENTITIES],
-  logging: process.env.TYPEORM_LOGGING as
-    | boolean
-    | 'all'
-    | ('query' | 'schema' | 'error' | 'warn' | 'info' | 'log' | 'migration')[],
-  logger: process.env.TYPEORM_LOGGER as
-    | 'debug'
-    | 'advanced-console'
-    | 'simple-console'
-    | 'file',
-  synchronize: Boolean(process.env.TYPEORM_SYNCHRONIZE),
-  bigNumberStrings: Boolean(process.env.TYPEORM_BIG_NUMBER_STRING),
-  // dropSchema: Boolean(process.env.TYPEORM_DROP_SCHEMA),
-  // cache: Boolean(process.env.TYPEORM_CACHE),
-});
-
 @Module({
   imports: [
     envConfigFilePath(),
--- src/working-hours/entities/working-hour.entity.ts
+++ src/working-hours/entities/working-hour.entity.ts
@@ -1,1 +1,46 @@
-export class WorkingHour {}
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { User } from '../../user/user.entity';
+
+@Entity()
+export class WorkingHour extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @OneToOne(() => User)
+  @JoinColumn()
+  user: User;
+
+  @Column({
+    type: 'date',
+    default: null,
+    unique: true,
+  })
+  date: Date | string;
+
+  @Column({
+    type: 'time',
+    default: null,
+  })
+  startTimeOfWork: Date | string;
+
+  @Column({
+    type: 'time',
+    default: null,
+  })
+  endTimeOfWork: Date | string;
+
+  @Column({
+    type: 'varchar',
+    length: 200,
+    default: null,
+  })
+  workDescription: string;
+}
